f55c96cc25e15d9972eae461710d680c379646f5
wyixin <single.w.1992@gmail.com>
2014-11-05 00:56:15 -0500

    rename & add test file

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+require "index.php";
+// Test case 1
+// echo HTTP_100
+// print Continue
+// var_dump(Http.100);
+
+// Test case 2
+// echo HTTP\Continue
+// print 100
+// $http = new Http();
+// var_dump($http->continue);
+//var_dump($statusCode['100']);
+var_dump(Http\_Continue);
+var_dump(Http\_Switching_Protocols);
+var_dump(Http\Switching_Protocols);
+var_dump(Http\Ok);
